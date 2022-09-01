07b529bd9fb7dc80c57f71d3d55dea2c0ddf3c12
A Tomzynska <atomzynska@outlook.com>
2022-09-01 14:21:58 -0700

    first commit

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -1,5 +1,5 @@
 import RestaurantsDAO from "../dao/restaurantsDAO.js"
-
+//github
 export default class RestaurantsController {
   static async apiGetRestaurants(req, res, next) {
     const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
